d31a00ca9387cf718b2f5d2cf9d4391783ab54b1
Chris <chris@codecomposer.co.uk>
2015-07-30 23:51:25 +0100

    Added ACF friendly search page

--- components/functions/functions-acf.php
+++ components/functions/functions-acf.php
@@ -3,6 +3,65 @@
  * Include any Options page setup or field set installations here
  */
 
+/**
+ * Add Join to search query
+ * Part 1 of 3 part functions to add ACF fields to search page results
+ *
+ * @param $join
+ * @return mixed
+ */
+function acf_search_join( $join ) {
+    global $wpdb;
+
+    if ( is_search() && is_main_query() ) {
+        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+    }
+
+    return $join;
+}
+add_filter('posts_join', 'acf_search_join' );
+
+/**
+ * Add meta value to where query
+ * Part 2 of 3 part functions to add ACF fields to search page results
+ *
+ * @param $where
+ * @return mixed
+ */
+function acf_search_where( $where ) {
+    global $pagenow, $wpdb;
+
+    if ( is_search() && is_main_query() ) {
+        $where = preg_replace(
+            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+
+        //Make sure we don't find results for ACF generated meta_value -> ACF field entries
+        $where = " AND (".$wpdb->postmeta.".meta_value NOT REGEXP 'field_[a-z0-9]{13}')" . $where;
+    }
+
+    return $where;
+}
+add_filter( 'posts_where', 'acf_search_where' );
+
+/**
+ * Force distinct on where query
+ * Part 3 of 3 part functions to add ACF fields to search page results
+ *
+ * @param $where
+ * @return string
+ */
+function acf_search_distinct( $where ) {
+    global $wpdb;
+
+    if ( is_search() && is_main_query() ) {
+        return "DISTINCT";
+    }
+
+    return $where;
+}
+add_filter( 'posts_distinct', 'acf_search_distinct' );
+
 //Add extra options pages to ACF
 if(function_exists('acf_add_options_page')) {
 
---
+++ search.php
@@ -0,0 +1,45 @@
+<?php get_header(); ?>
+
+    <main role="main" id="<?php echo get_post_type(); ?>-archive" class="clearfix">
+        <div class="wrap">
+            <?php if (have_posts()) : ?>
+                <h2>Search results for <?php the_search_query(); ?></h2>
+                <?php while (have_posts()) : the_post(); ?>
+
+                    <article>
+                        <header>
+                            <h1><?php the_title(); ?></h1>
+                        </header>
+                        <?php the_excerpt(); ?>
+                        <a href="<?php the_permalink(); ?>" class="button">View More</a>
+                    </article>
+
+                <?php endwhile; ?>
+            <?php else: ?>
+                <h2>Sorry no results for <?php the_search_query(); ?></h2>
+            <?php endif; ?>
+        </div>
+    </main>
+
+    <?php
+    if(have_posts()) :
+        global $wp_query;
+        $big = 999999999;
+        $paginate_links = paginate_links(array(
+            'base'      => str_replace($big, '%#%', get_pagenum_link($big)),
+            'current'   => max(1, get_query_var('paged')),
+            'total'     => $wp_query->max_num_pages,
+            'mid_size'  => 2,
+            'prev_next' => true,
+            'prev_text' => 'Previous',
+            'next_text' => 'Next'
+        ));
+        if ($paginate_links) {
+            echo '<nav class="pagination">';
+            echo $paginate_links;
+            echo '</nav>';
+        }
+    endif;
+    ?>
+
+<?php get_footer(); ?>
