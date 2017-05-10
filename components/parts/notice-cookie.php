5e7f38fd2872a94f8747447186862273fa11cb96
Chris Morris <chris@ahoy.co.uk>
2017-05-10 12:47:40 +0100

    Streamlined cookie notice

--- components/parts/notice-cookie.php
+++ components/parts/notice-cookie.php
@@ -1,8 +1,6 @@
 <?php if (!isset($_COOKIE['cookie-notice'])): ?>
     <div class="cookie-notice">
-        <h2><?php _e('This website uses cookies', 'tmp') ?></h2>
-        <p><?php _e('This website uses cookies to improve user experience. By using our website you consent to all cookies in accordance with our Cookie Policy.', 'tmp') ?></p>
+        <p><?php _e('This website uses cookies to improve user experience. By using our website you consent to the storage of cookies from this website.', 'tmp') ?></p>
         <a href="#" class="agree button"><?php _e('I agree', 'tmp') ?></a>
-        <a href="/cookies/" title="<?php _e('Cookie policy', 'tmp') ?>" class="more button"><?php _e('Read more', 'tmp') ?></a>
     </div>
 <?php endif; ?>
