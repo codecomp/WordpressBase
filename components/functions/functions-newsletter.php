6319183d8db77ac6080800504ab7ab70487dbe83
Chris Morris <chris@ahoy.co.uk>
2018-05-21 11:41:21 +0100

    Removed old newsletter connection code to be replaced with site specific, ideally composer driven conections

--- components/functions/functions-newsletter.php
+++ components/functions/functions-newsletter.php
@@ -5,7 +5,7 @@
  *********************************************************************/
 
 /**
- * Newsletter sign up function for Campaign monitor and Mail chimp
+ * Newsletter sign up function
  *
  * @param $email
  * @param $name
@@ -25,49 +25,9 @@ function newsletter_signup($email, $name, $custom_fields=array()){
     if (!filter_var($email, FILTER_VALIDATE_EMAIL))
         respond_and_close(false, __('Email Address Invalid', 'tmp'));
 
-    $is_mailchimp = strpos('-', $api_key);
+    // Handle submission
 
-    if ( !$is_mailchimp ) {
-        // CampaignMonitor
-
-        $fields = array(
-            'EmailAddress' 	=> $email,
-            'Name' 			=> $name,
-            'Resubscribe'   => true,
-            'customFields'  => $custom_fields
-        );
-
-        $api = 'https://api.createsend.com/api/v3.1/subscribers/' . $list_id .'.json?pretty=true';
-    } else {
-        // MailChimp
-        $name = explode(' ', $name);
-
-        $fields = array(
-            'email_address' => $email,
-            'status' => 'subscribed',
-            'merge_fields' => array(
-                'FNAME' => $name[0],
-                'LNAME' => isset($name[1]) ? $name[1] : ''
-            ),
-        );
-
-        foreach( $custom_fields as $k => $v ){
-            $fields['merge_fields'][$k] =$v;
-        }
-
-        $api_region = explode('-', $api_key);
-
-        $api = 'https://' . $api_region[1] . '.api.mailchimp.com/3.0/lists/' . $list_id .'/members/';
-    }
-
-    $json_data = json_encode($fields);
-
-    if ( !$is_mailchimp )
-        $response = curl_fetch($api, $api_key, 'x', $json_data);
-    else
-        $response = curl_fetch($api, 'x', $api_key, $json_data);
-
-    respond_and_close(true, $response);
+    respond_and_close();
 }
 
 /**
