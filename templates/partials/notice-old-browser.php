11e8118d283d8abcbb27dbd7d2c53336cf123c32
Chris Morris <chris@ahoy.co.uk>
2018-05-23 15:22:06 +0100

    Updated old browser notice for BEM structure

--- templates/partials/notice-old-browser.php
+++ templates/partials/notice-old-browser.php
@@ -1,1 +1,5 @@
-<!--[if lt IE 9]><p class="browser-outdated"><?php _e('You are using an <strong>extremely outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a>.', 'tmp'); ?></p><![endif]-->
+<!--[if lt IE 9]>
+    <div class="browser-outdated">
+        <p class="browser-outdated__text"><?php _e('You are using an <strong>extremely outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a>.', 'tmp'); ?></p>
+    </div>
+<![endif]-->
