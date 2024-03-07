5ad62a40d062025ef24774267c6ac071f4af1dc2
codecomp <chris@codecomposer.co.uk>
2024-03-07 16:51:19 +0000

    Set consistant letter spacing for config files

--- webpack.config.js
+++ webpack.config.js
@@ -9,145 +9,135 @@ const CopyPlugin = require('copy-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = (env, argv) => {
-  const isDevMode = argv.mode === 'development';
+    const isDevMode = argv.mode === 'development';
 
-  return {
-    entry: {
-        main: [
-          './assets/js/main.js',
-          './assets/css/main.scss'
-        ],
-        admin: ['./assets/css/admin-styles.scss'],
-        adminEditor: ['./assets/css/admin-editor-styles.scss'],
-    },
-    output: {
-        filename: 'js/[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(scss|css)$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: isDevMode,
-              },
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                sourceMap: isDevMode,
-              },
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: isDevMode,
-                implementation: require('sass'),
-              },
-            },
-          ],
+    return {
+        entry: {
+            main: ['./assets/js/main.js', './assets/css/main.scss'],
+            admin: ['./assets/css/admin-styles.scss'],
+            adminEditor: ['./assets/css/admin-editor-styles.scss'],
         },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          enforce: 'pre',
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  ['@babel/preset-env', { modules: false }],
-                  '@babel/preset-modules'
+        output: {
+            filename: 'js/[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(scss|css)$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: isDevMode,
+                            },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: isDevMode,
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: isDevMode,
+                                implementation: require('sass'),
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    enforce: 'pre',
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [['@babel/preset-env', { modules: false }], '@babel/preset-modules'],
+                            },
+                        },
+                        {
+                            loader: 'eslint-loader',
+                            options: {},
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].css',
+            }),
+
+            isDevMode &&
+                new StylelintPlugin({
+                    files: '**/*.scss',
+                }),
+
+            isDevMode &&
+                new BrowserSyncPlugin({
+                    host: 'localhost',
+                    proxy: {
+                        target: 'http://wordpress.local/',
+                    },
+                    files: ['**/*.php', '**/*.twig', 'dist/css/main.scss'],
+                    injectCss: true,
+                    open: false,
+                    notify: false,
+                }),
+
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'assets/images'),
+                        to: 'images',
+                    },
                 ],
-              },
-            },
-            {
-              loader: 'eslint-loader',
-              options: {
-              },
-            },
-          ],
-        }
-      ],
-    },
-    plugins: [
+            }),
 
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css'
-        }),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'assets/fonts'),
+                        to: 'fonts',
+                    },
+                ],
+            }),
 
-        isDevMode && new StylelintPlugin({
-          files: '**/*.scss',
-        }),
-
-        isDevMode && new BrowserSyncPlugin({
-          host: 'localhost',
-          proxy: {
-            target: 'http://wordpress.local/'
-          },
-          files: [
-            '**/*.php',
-            '**/*.twig',
-            'dist/css/main.scss',
-          ],
-          injectCss: true,
-          open: false,
-          notify: false
-        }),
-
-        new CopyPlugin ({
-          patterns: [
-            {
-              from: path.resolve(__dirname, 'assets/images'),
-              to: 'images'
-            },
-          ],
-        }),
-        
-        new CopyPlugin ({
-          patterns: [
-            {
-              from: path.resolve(__dirname, 'assets/fonts'),
-              to: 'fonts'
-            },
-          ],
-        }),
-        
-        new CopyPlugin ({
-          patterns: [
-            {
-              from: path.resolve(__dirname, 'assets/favicons'),
-              to: 'favicons'
-            },
-          ],
-        }),
-
-    ].filter(Boolean),
-    optimization: {
-      minimize: true,
-      minimizer: [
-        !isDevMode && new TerserPlugin(),
-        new ImageMinimizerPlugin({
-          generator: [{
-            type: "asset",
-            implementation: ImageMinimizerPlugin.imageminGenerate,
-            options: {
-              plugins: [
-                ['imagemin-optipng'],
-                ['imagemin-mozjpeg'],
-                ['imagemin-gifsicle'],
-                ['imagemin-svgo'],
-              ],
-            },
-          }]
-        })
-      ].filter(Boolean),
-    },
-    devtool: isDevMode ? 'source-map' : false,
-  };
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'assets/favicons'),
+                        to: 'favicons',
+                    },
+                ],
+            }),
+        ].filter(Boolean),
+        optimization: {
+            minimize: true,
+            minimizer: [
+                !isDevMode && new TerserPlugin(),
+                new ImageMinimizerPlugin({
+                    generator: [
+                        {
+                            type: 'asset',
+                            implementation: ImageMinimizerPlugin.imageminGenerate,
+                            options: {
+                                plugins: [
+                                    ['imagemin-optipng'],
+                                    ['imagemin-mozjpeg'],
+                                    ['imagemin-gifsicle'],
+                                    ['imagemin-svgo'],
+                                ],
+                            },
+                        },
+                    ],
+                }),
+            ].filter(Boolean),
+        },
+        devtool: isDevMode ? 'source-map' : false,
+    };
 };
-
