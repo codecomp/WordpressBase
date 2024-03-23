1cddc7719715caead899479378b4c1c8389cd8eb
codecomp <chris@codecomposer.co.uk>
2024-03-23 20:54:24 +0000

    Removed no longer needed Js function due to native JS closest function

--- assets/js/modules/utils.js
+++ assets/js/modules/utils.js
@@ -133,30 +133,3 @@ export function isJsonString(str) {
     }
     return true;
 }
-
-/**
- * Get the closest element of a given element by class
- *
- * Take an element (the first param), and traverse the DOM upward from it
- * until it hits the element with a given class name (second parameter).
- * This mimics jQuery's `.closest()`.
- *
- * @param  {element} el    The element to start from
- * @param  {string}  clazz The class name
- * @return {element}       The closest element
- */
-export function closestByClass(el, clazz) {
-    // Traverse the DOM up with a while loop
-    while (!el.classList.contains(clazz)) {
-        // Increment the loop to the parent node
-        el = el.parentNode;
-        if (!el) {
-            return null;
-        }
-    }
-    // At this point, the while loop has stopped and `el` represents the element that has
-    // the class you specified in the second parameter of the function `clazz`
-
-    // Then return the matched element
-    return el;
-}
